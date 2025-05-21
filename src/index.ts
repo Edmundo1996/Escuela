fa2e8375af36376ec2b1f730051c0abc2c24b409
Edmundo <edmundo_konker@hotmail.com>
2025-05-21 00:18:08 -0600

    tarea1

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import morgan from 'morgan';
+import routes from './routes/router'; // <--- sin .ts
+
+const app = express();
+const PORT  = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
